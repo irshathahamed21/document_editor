59bfc40e40dcfaa5aa84d33c3d62d79938d50b9e
irshathahamed21 <21irshathkhan@gmail.com>
2022-04-22 16:30:38 +0530

    updated TextEditor file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import TextEditor from "./TextEditor";
 
 
+import {BrowserRouter as Router, Switch, Route,Redirect} from "react-router-dom"
+
+import {v4 as uuidV4} from "uuid"
+
 function App() {
   return (
-    <div>
-     <TextEditor/>
-    </div>
+    <Router>
+      <Switch>
+      <Route exact path = "/">
+          <Redirect to = {`/documents/${uuidV4()}`}/>
+      </Route>
+      <Route exact path = "/documents/:id">
+      <TextEditor/>
+      </Route>
+
+      </Switch>
+
+    
+
+    </Router>
   );
 }
 
--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,8 +1,95 @@
-import {useCallback} from 'react'
+import {useCallback,useEffect,useState} from 'react'
 import Quill from "quill";
 import "quill/dist/quill.snow.css"
+import {io} from "socket.io-client"
+import {useParams} from "react-router-dom"
+
+const SAVE_INTERVAL_MS =2000
 
 export default function TextEditor() {
+    const {id:documentId} = useParams()
+
+    const[socket,setSocket] = useState()
+    const[quill, setQuill] = useState()
+
+
+    useEffect(()=> {
+        const s = io("http://localhost:3001")
+        setSocket(s)
+
+        return () => {
+            s.disconnect()
+        }
+    },[])
+
+    useEffect(()=> {
+        if(socket == null ||quill ==null) {
+            return
+        }
+
+        socket.once("load-document", document => {
+            quill.setContents(document)
+            quill.enable()
+        })
+        socket.emit("get-document",documentId)
+
+    },[socket,quill,documentId])
+
+
+
+    // document saving after 2 seconds
+    useEffect(()=> {
+        if(socket == null ||quill ==null) {
+            return
+        }
+
+        const interval = setInterval(()=> {
+            socket.emit("save-document", quill.getContents())
+
+        },SAVE_INTERVAL_MS)
+
+        return () => {
+            clearInterval(interval)
+        }
+
+
+    },[socket,quill])
+
+    useEffect(()=> {
+
+        if(socket ==null || quill == null){
+            return
+        }
+        const handler = (delta) =>{
+                quill.updateContents(delta)
+        }
+        socket.on("receive-changes", handler)
+
+        return()=> {
+            socket.off("receive-changes", handler)
+        }
+
+    },[socket, quill])
+
+    useEffect(()=> {
+
+        if(socket ==null || quill == null){
+            return
+        }
+        const handler = (delta, oldDelta, source) =>{
+            if(source !== "user") {
+                return
+            }
+            socket.emit("send-changes",delta)
+
+        }
+        quill.on("text-change", handler)
+
+        return()=> {
+            quill.off("text-change", handler)
+        }
+
+    },[socket, quill])
 
     const wrapperRef = useCallback((wrapper)=> {
         if(wrapper ==null) return 
@@ -10,7 +97,10 @@ export default function TextEditor() {
         wrapper.innerHTML = ""
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill(editor, {theme:"snow"})
+       const q = new Quill(editor, {theme:"snow"})
+       q.disable()
+       q.setText("Loading...")
+       setQuill(q)
 
     },[])
 
